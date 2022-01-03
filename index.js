465c871b33bbf8604828e54ad1ca09f2dd2336d6
Gerdeth Daniels <gerdethwickham@gmail.com>
2022-01-03 14:00:31 -0500

    imports

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-const songs= ["all my life", "Crazy","love"];
 
-const mood =["sad", "mad", "happy"];
 
-let everything =songs[1]
+class Stuff extends React.Component{
+    // const songs= ["all my life", "Crazy","love"];
 
-console.log(everything)
+    // const mood =["sad", "mad", "happy"];
+
+    // let everything =songs[1]
+
+    render(){
+        return <h1>Hello!</h1>;
+    }
+
+};
+ReactDOM.render(
+    <Stuff/>,
+    document.getElementById("app")
+);
