c948ccfff7357b30e98376415f59b4ded6f8aed5
Gerdeth Daniels <gerdethwickham@gmail.com>
2022-01-03 13:18:22 -0500

    arrays

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom'
 
-class Random extends React.Component{
-    const songs ={Lady in red, Beauty and the Beast, Hero}
-    render(){
-        return <h1>{songs}</h1>
-    }
-}
-ReactDOM.render(
-    <Random/>,
-    document.getElementById('app')
-);
+const songs= ["all my life", "Crazy","love"];
+
+const mood =["sad", "mad", "happy"];
+
+let everything =songs[1]
+
+console.log(everything)
